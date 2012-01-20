dde4bf27d4e7158e1b419a069036709bfc52ae7f
hborders <hborders@mail.win.org>
2012-01-20 16:40:17 -0600

    Created project.

---
+++ CoreDataNullifyDeleteActionBug/HBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HBAppDelegate.h
+//  CoreDataNullifyDeleteActionBug
+//
+//  Created by Heath Borders on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreDataNullifyDeleteActionBug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataNullifyDeleteActionBug
+//
+//  Created by Heath Borders on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HBAppDelegate class]));
+    }
+}
